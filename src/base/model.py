544f608a3a0f800b62c6cd57dcef0326172f6aa0
Ycaro Sales <ycaro00287@gmail.com>
2025-07-18 12:33:16 -0300

    feat: Finished Model template class

--- src/base/model.py
+++ src/base/model.py
@@ -1,60 +1,55 @@
-from datetime import date
+from dataclasses import field
+from datetime import datetime
 from uuid import UUID, uuid4
-from typing import Dict, List, Tuple, NewType
-import peewee
+from typing import ClassVar, Dict, Any, Unpack, Self
+from pydantic import BaseModel, ConfigDict, ValidationError
 
 
-class Model:
-    _items: Dict[UUID, "Model"] = {}
-    FIELDS: Dict[str, type] = {}
+class Model(BaseModel):
+    instances: ClassVar[Dict[UUID, Self]]
 
-    def __init__(self) -> None:
-        self.id: UUID = uuid4()
-        self.created_at: date = date.today()
-        for k, v in self.FIELDS.items():
-            self.__setattr__(k, v)
+    id: UUID = field(default_factory=uuid4)
+    created_at: datetime = datetime.now()
 
-    @classmethod
-    def _create(cls, **kwargs) -> "Model":
-        temp = cls()
-        for i in cls.FIELDS.keys():
-            pass
-        cls._items[temp.id] = temp
-        return temp
+    def __init_subclass__(cls, **kwargs: Unpack[ConfigDict]):
+        cls.instances = {}
+        super().__init_subclass__(**kwargs)
 
     @classmethod
-    def create(cls) -> "Model":
-        return cls._create()
+    def create(cls, /, **data: Any) -> Self | None:
+        try:
+            instance = cls(**data)
+            cls.instances[instance.id] = instance
+        except (ValidationError, TypeError) as e:
+            print(f"Invalid model. Error: {e}")
+            return None
+
+        return instance
 
     @classmethod
     def delete(cls, id: UUID) -> None:
-        if cls._items.get(id) is None:
+        if cls.instances.get(id) is None:
             return None
-        del cls._items[id]
-
-    # TODO: Atualmente essa funcao atualiza a instancia inteira mesmo
-    # se tiver um atributo nao encontrado
-    def update(self, **kwargs):
-        for i, v in kwargs.items():
-            if hasattr(self, i):
-                self.__dict__[i] = v
-            else:
-                print(f"<{self.__class__.__name__}> doesnt have attribute {i}")
-        print(f"updated {self.__class__.__name__}")
+        del cls.instances[id]
 
     @classmethod
-    def get(cls, id: UUID):
-        return cls._items.get(id)
+    def update(cls, id: UUID, **kwargs):
+        try:
+            if model := cls.instances.get(id):
+                model.update(**kwargs)
+                return model
+            else:
+                return None
+
+        except (TypeError, ValidationError) as e:
+            print(f"Invalid Model. Error: {e}")
+            return None
+
+    @classmethod
+    def get(cls, id: UUID) -> Self | None:
+        return cls.instances.get(id)
 
     # TODO: Criar filtros
     @classmethod
-    def list(cls, pagination: int = 50):
-        models = []
-        count = 1
-        for i in cls._items.items():
-            if count > pagination:
-                break
-            models.append(i)
-            count += 1
-
-        return cls._items.items()
+    def list(cls):
+        return cls.instances.values()
